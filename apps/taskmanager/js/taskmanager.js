631519a3db967383be4691b76cdd017b073bed7a
wdeng <wdeng@mozilla.com>
2012-12-21 14:55:57 +0800

    improve taskmanager

--- apps/taskmanager/js/taskmanager.js
+++ apps/taskmanager/js/taskmanager.js
@@ -13,30 +13,50 @@ var TaskManager = {
 
   loadApps: function tm_loadApps() {
     var self = this;
-
-    navigator.mozApps.mgmt.getAll().onsuccess = function mozAppGotAll(evt) {
+    container.innerHTML = "";
+    navigator.mozApps.mgmt.getAllRunning().onsuccess = function mozAppGotAll(evt) {
       var apps = evt.target.result;
       var listFragment = document.createDocumentFragment();
       apps.forEach(function(app) {
-        if (!app.removable)
-          return;
+        //if (!app.removable)
+        //  return;
         self._apps.push(app);
 
         var item = document.createElement('li');
-        var name = document.createTextNode(app.manifest.name);
+        var name;
+        var entryPoints = app.manifest.entry_points;
+        if (entryPoints) {
+          for (var ep in entryPoints) {
+            var currentEp = entryPoints[ep];
+            if ( app.origin.indexOf(currentEp.launch_path) != -1) {
+              name = document.createTextNode(currentEp.locales["en-US"].name);
+              break;
+            }
+          }
+        }else {
+          name = document.createTextNode(app.manifest.name);
+        }
         var bt = document.createElement("input");
         bt.setAttribute('type','button');
         bt.setAttribute('value','close');
         bt.onclick = function(evt) { 
-         alert (app.manifest.name);
+         navigator.mozApps.mgmt.close(app.origin);
+         TaskManager.loadApps();
         }
         item.appendChild(name);
         item.appendChild(bt);
         listFragment.appendChild(item);
+        container.appendChild(listFragment);
       });
       self.container.appendChild(listFragment);
+    },
+
+    navigator.mozApps.mgmt.getAllRunning().onerror = function ()
+    {
+      alert("error");
     }
   }
 };
 
 TaskManager.init();
+
